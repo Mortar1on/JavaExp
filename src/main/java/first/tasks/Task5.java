5a952bf3364a55117686ce222acaccc04f8e1fe1
Mortar1on <mortarion018@gmail.com>
2020-05-06 14:46:56 +0300

    Task5 reworked

--- src/main/java/first/tasks/Task5.java
+++ src/main/java/first/tasks/Task5.java
@@ -1,38 +1,46 @@
 package first.tasks;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class Task5 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(reader.readLine());
-        String[] array = reader.readLine().trim().split(" ");
+    private static final Set<Character> AGREEABLE = new HashSet<>(Arrays.asList('B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',
+            'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'));
+    private static final Set<Character> VOWEL = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'));
 
-        int sum = 0;
-        int lowercase;
-        int uppercase;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countProcessingWords = Integer.parseInt(scanner.nextLine());
+        LinkedHashSet<String> words = new LinkedHashSet<>();
 
-        for (int i = 0; i < count; i++){
+        for (int i = 0; i < countProcessingWords; i++) {
+            words.add(scanner.next());
+        }
 
-            if ( array[i].matches( "^[a-zA-Z]+$" ) && (array[i].length() % 2 == 0) ) {
-                lowercase=0;
-                uppercase=0;
-                for (int j = 0; j < array[i].length(); j++){
-                    if (Character.toString(array[i].charAt(j)).matches("(?i:[aeiouy]).*")){
-                        lowercase++;
-                    }else{
-                        uppercase++;
-                    }
-                }
-                if (lowercase == uppercase) {
-                    sum++;
+        int result = solve(words);
+
+        System.out.println(result);
+    }
+
+     static int solve(LinkedHashSet<String> words) {
+        int result = 0;
+        for (String word : words) {
+            int vowels = 0;
+            int agreeables = 0;
+            boolean onlyLatin = true;
+            for (Character c : word.toCharArray()) {
+                if (VOWEL.contains(c)) {
+                    vowels++;
+                } else if (AGREEABLE.contains(c)) {
+                    agreeables++;
+                } else {
+                    onlyLatin = false;
                 }
             }
-
+            if (onlyLatin && vowels == agreeables) {
+                result++;
+            }
         }
-        System.out.println(sum);
+        return result;
     }
 }
